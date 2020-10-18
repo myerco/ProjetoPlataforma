654eab0f4cb3432ce65b4f2796dabd659cc0ba6b
Marco <myerco@gmail.com>
2020-10-17 20:25:55 -0400

    feat: Adicionando o projeto inicial

---
+++ Source/ProjetoPlataforma/ProjetoPlataforma.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjetoPlataforma.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjetoPlataforma, "ProjetoPlataforma" );
---
+++ Source/ProjetoPlataforma/ProjetoPlataformaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjetoPlataformaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJETOPLATAFORMA_API AProjetoPlataformaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
