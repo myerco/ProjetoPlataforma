3328ad6274826607f581351c3f8a9ffbdf98e8d3
Marco <myerco@gmail.com>
2020-11-21 16:17:55 -0400

    feat: Inicializando gameinstance em C++

---
+++ Source/ProjetoPlataforma/core/PlataformaGameInstance.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlataformaGameInstance.h"
+
+void UPlataformaGameInstance::Init()
+{
+	//UE_LOG(LogTemp, Warning, TEXT("Classe do menu %s"), *MenuClass->GetName());
+	UE_LOG(LogTemp, Warning, TEXT("Inicializando o menu"));
+}
